53325f91a37167c08e89bd2d9e878e13c47e6b5e
Anne Gill <gill01730@gmail.com>
2018-03-20 12:20:39 +0000

    Connected S3

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -1,1 +1,8 @@
 from .base import *
+
+MEDIA_URL='/media/'
+MEDIA_ROOT='media'
+STATIC_URL = '/static/'
+STATIC_ROOT='staticfiles'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
